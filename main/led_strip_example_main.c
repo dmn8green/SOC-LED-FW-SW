71da73103d1be3a8467a2e7c85c24d175e48e52e
Bill McGuinness <bmcguinness@appliedlogix.com>
2023-08-14 11:47:24 -0400

    GPIO pin to 12 for demo board

--- main/led_strip_example_main.c
+++ main/led_strip_example_main.c
@@ -12,7 +12,7 @@
 
 
 #define RMT_LED_STRIP_RESOLUTION_HZ 10000000 // 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
-#define RMT_LED_STRIP_GPIO_NUM      0
+#define RMT_LED_STRIP_GPIO_NUM      12 
 
 #define EXAMPLE_LED_NUMBERS         24
 #define EXAMPLE_CHASE_SPEED_MS      10
