554995ca9a68f60abac792ac998c5bf584fc69f5
pat laplante <laplante.patrick@gmail.com>
2023-08-21 14:26:34 -0400

    Repl console is no longer on the default UART0.
    That is to separate log output with the REPL output.  However, log
    output always output to the uart associated with a task.
    
    On the final board we might want to output REPL to uart 0 along with all
    log.

--- main/ReplConsole/cmd_wifi.cpp
+++ main/ReplConsole/cmd_wifi.cpp
@@ -24,10 +24,22 @@ static void event_handler(void* arg, esp_event_base_t event_base,
         esp_wifi_connect();
         xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(__func__, "Got IPv4 event: Interface \"%s\" address: " IPSTR, esp_netif_get_desc(event->esp_netif), IP2STR(&event->ip_info.ip));
+        // if (!this->isOurNetif(TAG, event->esp_netif)) {
+        //     ESP_LOGW(TAG, "Got IPv4 from another interface \"%s\": ignored", esp_netif_get_desc(event->esp_netif));
+        //     return;
+        // }
+        // ESP_LOGI(TAG, "Got IPv4 event: Interface \"%s\" address: " IPSTR, esp_netif_get_desc(event->esp_netif), IP2STR(&event->ip_info.ip));
+        // this->ipAddr = event->ip_info.ip.addr;
+        // this->gwAddr = event->ip_info.gw.addr;
+        // ESP_LOGI(TAG, "WE ARE CONNECTED");
+
         xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
     }
 }
 
+esp_netif_t *sta_netif;
 static void initialise_wifi(void)
 {
     esp_log_level_set("wifi", ESP_LOG_WARN);
@@ -40,7 +52,7 @@ static void initialise_wifi(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_t *ap_netif = esp_netif_create_default_wifi_ap();
     assert(ap_netif);
-    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    sta_netif = esp_netif_create_default_wifi_sta();
     assert(sta_netif);
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     
@@ -102,7 +114,16 @@ static int connect(int argc, char **argv)
         ESP_LOGW(__func__, "Connection timed out");
         return 1;
     }
+
+    
     ESP_LOGI(__func__, "Connected");
+
+    esp_netif_ip_info_t ip_info;
+    esp_netif_get_ip_info(sta_netif, &ip_info);
+    printf("My IP: " IPSTR "\n", IP2STR(&ip_info.ip));
+    printf("My GW: " IPSTR "\n", IP2STR(&ip_info.gw));
+    printf("My NETMASK: " IPSTR "\n", IP2STR(&ip_info.netmask));
+
     return 0;
 }
 
--- main/ReplConsole/repl_console.h
+++ main/ReplConsole/repl_console.h
@@ -3,5 +3,5 @@
 #include <stdint.h>
 #include "esp_console.h"
 
-void repl_configure(uint16_t txPin, uint16_t rxPin, uint32_t baudRate);
+void repl_configure(uint16_t txPin, uint16_t rxPin, uint16_t channel, uint32_t baudRate);
 void repl_start(void);
