c0ffcb04471cd5dd4bf76d7922bafcee1c178ecd
Bill McGuinness <bmcguinness@appliedlogix.com>
2023-10-06 13:59:11 -0400

    Move LED count out of SPI module

---
+++ main/LED/Led.h
@@ -0,0 +1,16 @@
+//******************************************************************************
+/**
+ * @file Led.h
+ * @author Bill McGuinness (bmcguinness@appliedlogix.com)
+ * @brief Led definitions
+ * @version 0.1
+ * @date 2023-09-19
+ *
+ * @copyright Copyright MN8 (c) 2023
+ */
+//******************************************************************************
+#pragma once
+
+#define LED_STRIP_PIXEL_COUNT 32
+
+
