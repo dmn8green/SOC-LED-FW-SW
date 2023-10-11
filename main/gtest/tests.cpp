0518872c3600a7cb42d4e7e969c20c9f40e2a66d
Bill McGuinness <bmcguinness@appliedlogix.com>
2023-10-11 15:59:58 -0400

    Test charge animation automatically; Interactive mode now does in-animation charge down as well as up

--- main/gtest/tests.cpp
+++ main/gtest/tests.cpp
@@ -9,10 +9,15 @@
 #include "ChargingAnimation.h"
 #include "Utils/Colors.h"
 
+/* Helper function prototypes */
+static int blueLedCount (uint8_t *pLedPixels);
+static void showLed (uint8_t *pLedPixels, int ledIndex);
+static void showLedStripHeader(void);
+
 //******************************************************************************
 /**
- * @brief Test get/set of charge level; range limits enforced, etc.  
- * 
+ * @brief Test get/set of charge level; range limits enforced, etc.
+ *
  */
 
 TEST(animation, chargeLevelGetSet)
@@ -56,8 +61,6 @@ TEST(animation, chargeLevelLedsStatic)
 
     ASSERT_FALSE (testAnimate.get_charge_simulation());
 
-    uint8_t led_pixels[3*LED_STRIP_PIXEL_COUNT] = {0};
-
     // Remain agnostic towards algorithm, but validate a few
     // invariants as we loop through charge percentages of 0-100.
     //
@@ -70,6 +73,7 @@ TEST(animation, chargeLevelLedsStatic)
     uint32_t lastChargedLedCount = 0;
     for (int chargeLevel = 0; chargeLevel <= 100; chargeLevel++)
     {
+        uint8_t led_pixels[3*LED_STRIP_PIXEL_COUNT] = {0};
         testAnimate.set_charge_percent(chargeLevel);
         testAnimate.refresh (led_pixels, LED_STRIP_PIXEL_COUNT, 0);
         uint32_t chargedLedCount = testAnimate.get_led_charge_top_leds();
@@ -88,6 +92,53 @@ TEST(animation, chargeLevelLedsStatic)
 
         // Save for next iteration
         lastChargedLedCount = chargedLedCount;
+    }
+}
+
+//******************************************************************************
+/**
+ * @brief Test animated display at a few charge levels.
+ *
+ *        Counts total number of blue pixels on the LED bar.
+ *        This is not as throrough as a full pixel-by-pixel check
+ *        but still effective and easy to implement.
+ *
+ */
+TEST(animation, chargeLevelLedsDynamic)
+{
+    ChargingAnimation testAnimate;
+    ASSERT_FALSE (testAnimate.get_charge_simulation());
+
+    // Start charge level high enough that we expect three LEDs
+    // to be blue (base indicator and charge level indicator)
+    for (int chargeLevel = 30; chargeLevel <= 100; chargeLevel++)
+    {
+        ChargingAnimation testAnimate;
+        ASSERT_FALSE (testAnimate.get_charge_simulation());
+        testAnimate.set_quiet(true);
+
+        uint8_t led_pixels[3*LED_STRIP_PIXEL_COUNT] = {0};
+
+        testAnimate.reset(COLOR_BLUE, COLOR_WHITE, COLOR_BLUE, 100);
+        testAnimate.set_charge_percent(chargeLevel);
+        testAnimate.refresh (led_pixels, LED_STRIP_PIXEL_COUNT, 0);
+
+        // Make sure we are starting with normal blue LED count (usually 3)
+        int baseBlueCnt = CHARGE_BASE_LED_CNT + CHARGE_LEVEL_LED_CNT;
+        ASSERT_TRUE (blueLedCount (led_pixels) == baseBlueCnt);
+
+        int lastBlueCnt = baseBlueCnt;
+        for (int cycles = 0; cycles < 100; cycles++)
+        {
+            testAnimate.refresh (led_pixels, LED_STRIP_PIXEL_COUNT, 0);
+            // Should have one more blue pixel than last time, or it's cycled
+            // back to base (3 LEDs).
+            int blueCnt = blueLedCount (led_pixels);
+
+            ASSERT_TRUE (blueCnt == lastBlueCnt+1 || blueCnt == baseBlueCnt);
+
+            lastBlueCnt = blueCnt;
+        }
     }
 }
 
@@ -132,6 +183,34 @@ TEST(animation, chargeLevelPixels)
 
 }
 
+//******************************************************************************
+/**
+ * @brief  Count number of BLUE LEDs in an array. Helper function for
+ *         testing animation.
+ */
+static int blueLedCount (uint8_t *pLedPixels)
+{
+    int blueCount = 0;
+
+    if (pLedPixels != NULL)
+    {
+        for (int x = 0; x < LED_STRIP_PIXEL_COUNT; x++)
+        {
+            if ((pLedPixels[(3 * x) + 0] == 0x00) &&
+                (pLedPixels[(3 * x) + 1] == 0x00) &&
+                (pLedPixels[(3 * x) + 2] == 0xFF))
+            {
+                blueCount++;
+            }
+        }
+    }
+
+    //printf ("Counted %d blue LEDs\n", blueCount);
+    return blueCount;
+}
+
+
+
 
 
 //******************************************************************************
@@ -139,7 +218,7 @@ TEST(animation, chargeLevelPixels)
  * @brief Mostly for interactive testing, shows an ASCII representation 
  *        of a passed LED/Pixel.  
  */
-void showLED (uint8_t *pLedPixels, int ledIndex)
+static void showLed (uint8_t *pLedPixels, int ledIndex)
 {
     uint8_t g = *(pLedPixels + (ledIndex*3) + 0);
     uint8_t r = *(pLedPixels + (ledIndex*3) + 1);
@@ -198,8 +277,9 @@ void showLED (uint8_t *pLedPixels, int ledIndex)
  * @brief When displayed the LED bar, a header to indicate offsets. Fixed at 32 
  *        for now (TODO:)  
  */
-void showBarHeader(void)
+static void showLedStripHeader(void)
 {
+    printf ("LED BAR:                      1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 3 3\n");
     printf ("LED BAR:  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1\n");
 }
 
@@ -207,13 +287,13 @@ void showBarHeader(void)
 /**
  * @brief Basic ASCII representation of LED bar.
  */
-void showBar (uint8_t *pLedPixels)
+static void showLedStrip (uint8_t *pLedPixels)
 {
 
     printf ("LED BAR: ");
     for (int led=0; led < LED_STRIP_PIXEL_COUNT; led++)
     {
-        showLED(pLedPixels, led);
+        showLed(pLedPixels, led);
     }
     printf ("|\n");
 }
@@ -225,7 +305,7 @@ void showBar (uint8_t *pLedPixels)
  *        and see a (crude) display of "animation". Covid-driven test (no access 
  *        to an LED bar).
  */
-void interactive_charge (void)
+void interactive_mode (void)
 {
     char *string = NULL;
     size_t size = 0;
@@ -236,7 +316,14 @@ void interactive_charge (void)
     while (true)
     {
 
-        printf ("Enter charge level (0-100) or q to quit\n");
+        printf ("\nEnter an option:\n"
+                "  <num> to animate charge level (0-100)\n"
+                "  +<num> to bump charge level 5%% during animation\n"
+                "  -<num> to cut charge level 5%% during animation\n"
+                " 'q' or 'x' to quit\n"
+                " 's' to run charge simulation (long!)\n"
+                "led_test> ");
+
         chars_read = getline(&string, &size, stdin);
 
         if (chars_read < 0)
@@ -251,32 +338,88 @@ void interactive_charge (void)
             return;
         }
 
-        long chargePct = strtol (string, NULL, 10);
-        if (chargePct == LONG_MIN)
+        if (tolower(string[0]) == 's')
         {
-            printf ("Input error");
-            return;
+            // Simulated charge
+            ChargingAnimation testAnimate;
+            testAnimate.reset(COLOR_BLUE, COLOR_WHITE, COLOR_BLUE, 100);
+            uint8_t led_pixels[3*LED_STRIP_PIXEL_COUNT] = {0};
+            testAnimate.set_quiet (true);
+            testAnimate.set_charge_simulation(true);
+
+            showLedStripHeader();
+            int numCycles = 1100;
+            for (int c = 0; c < numCycles; c++)
+            {
+                testAnimate.refresh(led_pixels, LED_STRIP_PIXEL_COUNT, 0);
+                showLedStrip (led_pixels);
+            }
+
         }
-
-        printf ("charge sim:  %ld\n", chargePct);
-
-        ChargingAnimation testAnimate;
-        testAnimate.set_quiet (true);
-
-        uint8_t led_pixels[3*LED_STRIP_PIXEL_COUNT] = {0};
-        testAnimate.reset(COLOR_BLUE, COLOR_WHITE, COLOR_BLUE, 100);
-
-        testAnimate.set_charge_percent(chargePct);
-
-        // Try to show one full cycle of charge animation, but where possible 
-        // keep it on the same "page". Roughly the number of animated pixels 
-        // plus a few more to show the wrap-around. 
-        int numCycles = ((chargePct * LED_STRIP_PIXEL_COUNT) / 100) + 3;
-        showBarHeader();
-        for (int c = 0; c < numCycles; c++)
+        else
         {
-            testAnimate.refresh(led_pixels, LED_STRIP_PIXEL_COUNT, 0);
-            showBar(led_pixels);
+
+            // User enters a charge percentage; simulated animation
+            // is displayed.
+
+            // Option to change charge level during animation
+            bool bumpDuringAnimate = false;
+            bool cutDuringAnimate = false;
+
+            // Option to bump charge level during animation
+            if (tolower(string[0]) == '+')
+            {
+                bumpDuringAnimate = true;
+                string[0] = ' ';
+            }
+            if (tolower(string[0]) == '-')
+            {
+                cutDuringAnimate = true;
+                string[0] = ' ';
+            }
+
+
+            long chargePct = strtol (string, NULL, 10);
+            if (chargePct == LONG_MIN)
+            {
+                printf ("Input error");
+                return;
+            }
+
+            printf ("Animation of charge pct: %ld (bump during animate: %s)\n", chargePct, bumpDuringAnimate ? "yes":"no");
+
+            ChargingAnimation testAnimate;
+            testAnimate.set_quiet (true);
+
+            uint8_t led_pixels[3*LED_STRIP_PIXEL_COUNT] = {0};
+            testAnimate.reset(COLOR_BLUE, COLOR_WHITE, COLOR_BLUE, 100);
+
+            testAnimate.set_charge_percent(chargePct);
+
+            // Try to show one full cycle of charge animation, but where possible
+            // keep it on the same "page". Roughly the number of animated pixels
+            // plus a few more to show the wrap-around.
+            int numCycles = ((chargePct * LED_STRIP_PIXEL_COUNT) / 100) + 3;
+            showLedStripHeader();
+            for (int c = 0; c < numCycles; c++)
+            {
+                if (c == numCycles/2)
+                {
+                    // roughly haflway through. Adjust charge level
+                    // if requested.
+                    if (bumpDuringAnimate)
+                    {
+                        testAnimate.set_charge_percent(chargePct+5);
+                    }
+                    if (cutDuringAnimate)
+                    {
+                        testAnimate.set_charge_percent(chargePct-5);
+                    }
+                }
+
+                testAnimate.refresh(led_pixels, LED_STRIP_PIXEL_COUNT, 0);
+                showLedStrip (led_pixels);
+            }
         }
     }
 
@@ -286,10 +429,11 @@ void interactive_charge (void)
  
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
+
+    // See if interactive/demo mode is selected; if not just
+    // run unit tests normally (RUN_ALL_TESTS)
+
     bool interactive=false;
-
-    // See if interactive charging mode is selected
-
     int c;
     while ((c = getopt (argc, argv, "i")) != -1)
     switch (c)
@@ -304,7 +448,7 @@ int main(int argc, char **argv) {
     }
     else
     {
-        printf ("INTERACTIVE!\n");
-        interactive_charge();
+        printf ("INTERACTIVE MODE:\n");
+        interactive_mode();
     }
 }
