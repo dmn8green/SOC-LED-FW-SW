33779dd8aa6c53d4c12e175d3a2d6df4f0965bcf
pat laplante <laplante.patrick@gmail.com>
2024-03-10 20:29:17 -0400

    build: publishing built artifacts

--- utils/make_build_zip.sh
+++ utils/make_build_zip.sh
@@ -7,10 +7,14 @@ echo $sed
 
 # parse release from rev.h
 rev=$(/bin/cat main/rev.h)
-major=$(echo "$rev" | grep MAJOR | $sed 's/#[a-zA-Z _]*MAJOR\s*\([0-9]*\)/\1/')
-minor=$(echo "$rev" | grep MINOR | $sed 's/#[a-zA-Z _]*MINOR\s*\([0-9]*\)/\1/')
-build=$(echo "$rev" | grep PATCH | $sed 's/#[a-zA-Z _]*PATCH\s*\([0-9]*\)/\1/')
+major=$(echo "$rev" | grep MAJOR\  | $sed 's/#[a-zA-Z _]*MAJOR\s*\([0-9]*\)/\1/')
+echo $major
+minor=$(echo "$rev" | grep MINOR\  | $sed 's/#[a-zA-Z _]*MINOR\s*\([0-9]*\)/\1/')
+echo $minor
+build=$(echo "$rev" | grep PATCH\  | $sed 's/#[a-zA-Z _]*PATCH\s*\([0-9]*\)/\1/')
+echo $build
 env=$(echo "$rev" | grep ENV | $sed 's/#[a-zA-Z _]*ENV\s*\([0-9]*\)/\1/' | xargs)
+echo $env
 version=$major.$minor.$build-$env
 echo "$version"
 
@@ -31,19 +35,7 @@ id,filename,md5sum,flashaddress
 1,partition-table.bin,$(md5sum "${pkg_folder}/partition-table.bin" | sed -r 's:\\*([^ ]*).*:\1:'),0x8000
 2,ota_data_initial.bin,$(md5sum "${pkg_folder}/ota_data_initial.bin" | sed -r 's:\\*([^ ]*).*:\1:'),0xd000
 3,led_strip.bin,$(md5sum "${pkg_folder}/led_strip.bin" | sed -r 's:\\*([^ ]*).*:\1:'),0x10000
+
 EOT
 
-# echo "zip ${pkg_folder}.zip hcl-1.5.6/*"
-# echo "zip ${pkg_folder}.zip ${pkg_folder}/*"
-
 zip -j ${pkg_folder}.zip ${pkg_folder}/*
-#zip "${pkg_folder}.zip" hcl-1.5.6/* #"${pkg_folder}/*"
-
-#! Users/pat/.espressif/python_env/idf4.4_py3.9_env/bin/python
-#! ../../../esp/esp-idf-v4.4.0/components/esptool_py/esptool/esptool.py
-#! -p (PORT) -b 460800 --before default_reset --after hard_reset --chip esp32
-#! write_flash --flash_mode dio --flash_size detect --flash_freq 40m
-#! 0x1000 ../../../../../Volumes/wip/hcl-build/bootloader/bootloader.bin
-#! 0x8000 ../../../../../Volumes/wip/hcl-build/partition_table/partition-table.bin
-#! 0xd000 ../../../../../Volumes/wip/hcl-build/ota_data_initial.bin
-#! 0x10000 ../../../../../Volumes/wip/hcl-build/stattemp-moscow.bin
